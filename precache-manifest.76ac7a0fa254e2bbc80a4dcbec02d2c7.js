e53b6e715490d1768c88cdde0e45e53fbfc0b0ec
fireflower0 <fireflower0@hotmail.co.jp>
2019-04-21 14:02:40 +0900

    Updates

---
+++ precache-manifest.76ac7a0fa254e2bbc80a4dcbec02d2c7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0944f58fc406e23e94c",
+    "url": "/react-gh-pages-lisp/static/js/runtime~main.ed59056f.js"
+  },
+  {
+    "revision": "09631e74568a46253bf5",
+    "url": "/react-gh-pages-lisp/static/js/main.4db5bd90.chunk.js"
+  },
+  {
+    "revision": "f37ca99981df1bb0ad82",
+    "url": "/react-gh-pages-lisp/static/js/2.cfbe10c5.chunk.js"
+  },
+  {
+    "revision": "30eb3d32bddd9da166662b3841962928",
+    "url": "/react-gh-pages-lisp/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-gh-pages-lisp/precache-manifest.76ac7a0fa254e2bbc80a4dcbec02d2c7.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-gh-pages-lisp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
