b6f22bfe7e3b2577d3f553086e756e060066a05d
fireflower0 <fireflower0@hotmail.co.jp>
2019-05-01 00:48:59 +0900

    Add a common lisp articles

--- src/Pages/Programming/CommonLisp/commonlisp.jsx
+++ src/Pages/Programming/CommonLisp/commonlisp.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom'
 
+const Article = styled.article`
+  width: 100%;
+  padding: 2.5%;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  background-color: #FAF0E6;
+`;
+
 class CommonLisp extends React.Component {
+  makeList(link, title) {
+    return(
+      <Article>
+        <Link to={link}>{title}</Link>
+      </Article>
+    );
+  }
+
   render() {
     return (
       <div>
-        <Link to="/programming/commonlisp/game">Common Lisp ゲームプログラミング</Link>
+        <h1>Common Lisp</h1>
+        {this.makeList("/programming/commonlisp/gettingstarted", "Common Lisp 入門")}
+        {this.makeList("/programming/commonlisp/game", "ゲームプログラミング")}
       </div>
     );
   }
--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { MemoryRouter as Router, Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
-import { Nav, NavIcon } from 'react-sidenav';
-import { Icon } from 'react-icons-kit';
 import Sidebar from './Shared/SideBar/sidebar';
 import Home from './Pages/Home/home';
 import Blog from './Pages/Blog/blog';
 import Programming from './Pages/Programming/programming';
 import CommonLisp from './Pages/Programming/CommonLisp/commonlisp';
+import GettingStarted from './Pages/Programming/CommonLisp/GettingStarted/gettingstarted'
 import Game from './Pages/Programming/CommonLisp/Game/game';
 
 export const AppContainer = styled.div`
@@ -21,36 +20,10 @@ export const Body = styled.div`
   height: 100vh;
   width: 1000px;
   margin-right: auto;
-  margin-left : auto;
-`;
-
-const Text = styled.div`
-  padding-left: 8px;
+  margin-left: auto;
 `;
 
 class App extends React.Component {
-  SubMenuItem (child) {
-    var list = [];
-    for (let i = 0; i < child.length; i++){
-      list.push(<Nav key={i} id={child[i][0]}>
-                  <Text>{child[i][1]}</Text>
-                </Nav>);
-    }
-    return list;
-  }
-
-  MenuItem(parent, child) {
-    return (
-      <Nav id={parent[0]}>
-        <NavIcon>
-          <Icon icon={parent[1]} />
-        </NavIcon>
-        <Text>{parent[2]}</Text>
-        { child !== null ? this.SubMenuItem(child) : null }
-      </Nav>
-    );
-  }
-
   render() {
     return (
       <Router>
@@ -61,6 +34,7 @@ class App extends React.Component {
               <Route path='/home' component={Home} />
               <Route path='/blog' component={Blog} />
               <Route path='/programming/commonlisp/game' component={Game} />
+              <Route path='/programming/commonlisp/gettingstarted' component={GettingStarted} />
               <Route path='/programming/commonlisp' component={CommonLisp} />
               <Route path='/programming' exact component={Programming} />
               <Route path='/' exact component={Home} />
