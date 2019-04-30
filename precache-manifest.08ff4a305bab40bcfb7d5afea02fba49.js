2cba03ceb763d4fbfd5e5f1dcf04737b0fa1edb4
fireflower0 <fireflower0@hotmail.co.jp>
2019-05-01 00:50:51 +0900

    Updates

---
+++ precache-manifest.08ff4a305bab40bcfb7d5afea02fba49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa4764921b78e5ad48567de947d9bdcd",
+    "url": "/react-gh-pages-lisp/static/media/sample.fa476492.md"
+  },
+  {
+    "revision": "b97a46c249e9f4b0ecf9b09fa703d173",
+    "url": "/react-gh-pages-lisp/static/media/02-window.b97a46c2.md"
+  },
+  {
+    "revision": "ee9fee2335a5a154bd41149fb1b7e718",
+    "url": "/react-gh-pages-lisp/static/media/01-foreword.ee9fee23.md"
+  },
+  {
+    "revision": "cab27f221f93ac408847206e431a54e1",
+    "url": "/react-gh-pages-lisp/static/media/01-foreword.cab27f22.md"
+  },
+  {
+    "revision": "a0944f58fc406e23e94c",
+    "url": "/react-gh-pages-lisp/static/js/runtime~main.ed59056f.js"
+  },
+  {
+    "revision": "7d4ec343901709921759",
+    "url": "/react-gh-pages-lisp/static/js/main.cc0a1921.chunk.js"
+  },
+  {
+    "revision": "5c221a07c1397014d1c3",
+    "url": "/react-gh-pages-lisp/static/js/2.0e773385.chunk.js"
+  },
+  {
+    "revision": "14c20a98805a957cd024514dc343d467",
+    "url": "/react-gh-pages-lisp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages-lisp/precache-manifest.8c2c73a7103b137cbbd31294f39aa204.js"
+  "/react-gh-pages-lisp/precache-manifest.08ff4a305bab40bcfb7d5afea02fba49.js"
 );
 
 workbox.clientsClaim();
