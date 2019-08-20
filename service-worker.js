77ffb3cb077f551836a3d8ac8037b20e2eb8af8b
fireflower0 <fireflower0@hotmail.co.jp>
2019-08-20 23:26:59 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages-lisp/precache-manifest.6014988c490b965505e0c3abcf215aa0.js"
+  "/react-gh-pages-lisp/precache-manifest.20d3ebc900421069ea40832cc213f886.js"
 );
 
 workbox.clientsClaim();
