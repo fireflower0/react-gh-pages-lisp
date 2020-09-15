e24aead911134bf7d06d75c16303d56ac6f50242
fireflower0 <fireflower0@hotmail.co.jp>
2020-09-16 00:01:31 +0900

    Fixed a app and index

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,54 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
+import Sidebar from './Shared/SideBar';
+import { routes } from './routes';
 
-const Index = () => {
+const AppContainer = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  overflow-y: auto;
+`;
+
+const Body = styled.div`
+  height: 100vh;
+  width: 1000px;
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+const App = () => {
   useEffect(() => {
     document.title = "fireflower0's HomePage";
     document.body.style.margin = "0";
     document.body.style.backgroundColor = "#DCDCDC"; // Set the style
   }, []);
 
+  const makeRoutes = (route) => (
+    <Route
+      path={route.path}
+      exact={route.exact}
+      component={route.component}
+    />
+  );
+
   return (
-    <App />
+    <AppContainer>
+      <Router>
+        <Sidebar />
+        <Body>
+          <Switch>
+            {routes.map(route => makeRoutes(route))}
+          </Switch>
+        </Body>
+      </Router>
+    </AppContainer>
   );
 };
 
 ReactDOM.render(
-  <Index />,
+  <App />,
   document.getElementById('root')
 );
