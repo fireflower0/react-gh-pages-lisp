39567dec66a63cb1bd793cd56a674485ea8be1dd
fireflower0 <fireflower0@hotmail.co.jp>
2020-09-18 00:02:25 +0900

    Fixed a index in RaspberryPi

--- src/Pages/CommonLisp/RaspberryPi/index.jsx
+++ src/Pages/CommonLisp/RaspberryPi/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { makeArticle } from '../../utils';
 
-class RaspberryPi extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Raspberry Pi 電子工作</h1>
-        {makeArticle("Lチカ", require('./Articles/01-blink.md'))}
-        {/* {makeArticle("タクトスイッチ", require(''))} */}
-        {/* {makeArticle("センサー", require(''))} */}
-      </div>
-    );
-  }
-}
+const RaspberryPi = () => {
+  return (
+    <div>
+      <h1>Raspberry Pi 電子工作</h1>
+      {makeArticle("Lチカ", require('./Articles/01-blink.md'))}
+      {/* {makeArticle("タクトスイッチ", require(''))} */}
+      {/* {makeArticle("センサー", require(''))} */}
+    </div>
+  );
+};
 
 export default RaspberryPi;
