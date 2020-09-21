da948d6ddf4912c45ca1e1623e70e342a50ed664
fireflower0 <fireflower0@hotmail.co.jp>
2020-09-22 00:01:53 +0900

    Fixed a index in WebApp

--- src/Pages/CommonLisp/WebApp/index.jsx
+++ src/Pages/CommonLisp/WebApp/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { makeArticle } from '../../utils';
 
-class WebApp extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Webアプリケーション</h1>
-        {makeArticle("第1章 開発環境構築", require("./Articles/01-foreword.md"))}
-      </div>
-    );
-  }
-}
+const WebApp = () => {
+  const articleRoutes = [{
+    label: '第1章 開発環境構築',
+    url: require("./Articles/01-foreword.md"),
+  }];
+
+  return (
+    <div>
+      <h1>Webアプリケーション</h1>
+      {articleRoutes.map(route => makeArticle(route.label, route.url))}
+    </div>
+  );
+};
 
 export default WebApp;
