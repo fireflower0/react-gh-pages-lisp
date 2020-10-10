e8b178967244724d90bec11d8153c2e5e284e30d
fireflower0 <fireflower0@hotmail.co.jp>
2020-10-11 00:01:44 +0900

    Fixed a Game

--- src/Pages/CommonLisp/Game/index.jsx
+++ src/Pages/CommonLisp/Game/index.jsx
@@ -3,15 +3,19 @@ import { makeArticle } from '../../utils';
 
 const Game = () => {
   const articles = [{
+    id: 1,
     label: 'はじめに',
     route: require("./Articles/01-foreword.md"),
   }, {
+    id: 2,
     label: 'ウィンドウを表示する',
     route: require("./Articles/02-window.md"),
   }, {
+    id: 3,
     label: '文字列描画',
     route: require("./Articles/03-string-drawing.md"),
   }, {
+    id: 4,
     label: '2Dレンダリング',
     route: require("./Articles/04-2d-rendering.md"),
   }];
